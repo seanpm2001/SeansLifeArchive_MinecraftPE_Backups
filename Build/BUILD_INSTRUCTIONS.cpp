2e34c12b154e241164ca864a5442908bd0d334bc
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-24 19:12:13 -0700

    BUILD_INSTRUCTIONS.cpp
    The build instructions file for this directory.

---
+++ Build/BUILD_INSTRUCTIONS.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+// Start of script
+int main()
+{
+    // Starter info
+    std::cout << "Build instructions for GitHub/seanpm2001/SeansLifeArchive_MinecraftPE_Backup_Worlds_Farmtopia\n";
+    std::cout << "This repository is an archive of all versions of my Minecraft PE world: Farmtopia\n";
+    std::cout << "Before you can use these worlds, please make sure of the following things:\n";
+    // Requirements
+    std::cout << "* That your storage location for Minecraft is set to `external`";
+    std::cout << "* That your device is running Android (it might work on iOS, but I am not sure)";
+    std::cout << "* That you are running a matching Minecraft version (a listing of what version each backup is using is listed in another repository";
+    std::cout << "* That your device has at least 20-512 megabytes of free space, and at least 2 gigabytes of RAM";
+    // Start
+    std::cout << "This is a backup of all the versions of the world, according to my journaling structure.";
+    std::cout << "The worlds are sorted by a monthly ID, starting at 1,July 2019, 2,August 2019, etc... up to 21,March 2021";
+    std::cout << "To start using the world, locate your device storage and go to: games/com.mojang/MinecraftWorlds/ then cut/copy and paste the directory containing the world code 3hUlXT74VQA into this folder (you can also use the zip file and extract it) you do not need to remove extra files before running the game, just make sure not to do this while the game is running";
+    std::cout << "Please also make sure that there is not already another world in your MinecraftWorlds folder labeled Farmtopia.";
+    // Outro
+    std::cout << "Once installed, you will have a copy of the entire world. You can now change any setting you want, and do whatever you want to the world. If things go wrong, just delete it, and replace the backup";
+}
+// End of script
